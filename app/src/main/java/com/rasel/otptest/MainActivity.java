dd8390864772dc47985ecf2a235440eded9b607e
rasel <rasel.00x@gmail.com>
2021-09-14 10:05:26 +0600

    implemented SmsRetrieverClient for  otp code retrieve

--- app/src/main/java/com/rasel/otptest/MainActivity.java
+++ app/src/main/java/com/rasel/otptest/MainActivity.java
@@ -1,22 +1,28 @@
 package com.rasel.otptest;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.telephony.SmsMessage;
-import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import java.util.Arrays;
-import java.util.List;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.auth.api.phone.SmsRetriever;
+import com.google.android.gms.auth.api.phone.SmsRetrieverClient;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
-    ExampleBroadcastReceiver exampleBroadcastReceiver = new ExampleBroadcastReceiver();
     private TextView tvOtpCode;
+    private SmsBroadcastReceiver smsBroadcastReceiver;
+
+    private static final int REQ_USER_CONSENT = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,48 +33,96 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+
+      //  startSmsUserConsent();
+
+        SmsRetrieverClient client = SmsRetriever.getClient(this);
+        client.startSmsUserConsent(null);
+
+
+    }
+
+    private void startSmsUserConsent() {
+        SmsRetrieverClient client = SmsRetriever.getClient(this);
+
+        //We can add sender phone number or leave it blank
+        // I'm adding null here
+       /* client.startSmsUserConsent(null).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Toast.makeText(getApplicationContext(), "On Success", Toast.LENGTH_LONG).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getApplicationContext(), "On OnFailure", Toast.LENGTH_LONG).show();
+            }
+        });*/
+
+        client.startSmsUserConsent(null);
+    }
+
+    private void registerBroadcastReceiver() {
+        smsBroadcastReceiver = new SmsBroadcastReceiver();
+        smsBroadcastReceiver.smsBroadcastReceiverListener =
+                new SmsBroadcastReceiver.SmsBroadcastReceiverListener() {
+                    @Override
+                    public void onSuccess(Intent intent) {
+                        startActivityForResult(intent, REQ_USER_CONSENT);
+                    }
+
+                    @Override
+                    public void onFailure() {
+
+                    }
+                };
+        IntentFilter intentFilter = new IntentFilter(SmsRetriever.SMS_RETRIEVED_ACTION);
+        registerReceiver(smsBroadcastReceiver, intentFilter);
+    }
+
+    @Override
     protected void onStart() {
         super.onStart();
-        IntentFilter filter = new IntentFilter("android.provider.Telephony.SMS_RECEIVED");
-        registerReceiver(otpCodeReceiver, filter);
+        registerBroadcastReceiver();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        unregisterReceiver(otpCodeReceiver);
+        unregisterReceiver(smsBroadcastReceiver);
     }
-    private BroadcastReceiver otpCodeReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            if ("android.provider.Telephony.SMS_RECEIVED".equals(intent.getAction())) {
-                final Bundle bundle = intent.getExtras();
-                try {
-                    if (bundle != null) {
-                        final Object[] pdusObj = (Object[]) bundle.get("pdus");
-                        for (Object aPdusObj : pdusObj) {
-                            SmsMessage currentMessage = SmsMessage.createFromPdu((byte[]) aPdusObj);
-                            String senderNum = currentMessage.getDisplayOriginatingAddress();
-                            String message = currentMessage.getDisplayMessageBody();
-                            try {
 
-                                List<String> msg = Arrays.asList(message.split(":"));
-                                String code = msg.get(1).trim();
-                                tvOtpCode.setText(code);
-                                Log.d("rasel", "onReceive: messsage is : "+message);
-                                Log.d("rasel", "onReceive: messsage sender num : "+senderNum);
-                            } catch (Exception e) {
-                                e.printStackTrace();
-                            }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQ_USER_CONSENT) {
+            if ((resultCode == RESULT_OK) && (data != null)) {
+                //That gives all message to us.
+                // We need to get the code from inside with regex
+                String message = data.getStringExtra(SmsRetriever.EXTRA_SMS_MESSAGE);
+                //  tvOtpCode.setText(String.format("%s - %s", getString(R.string.received_message), message));
+                // tvOtpCode.setText( message);
+                //  getOtpFromMessage(message);
 
-                        }
-                    }else{
-                        Log.d("rasel", "onReceive: bunder is null");
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
+                // This will match any 6 digit number in the message
+                Pattern pattern = Pattern.compile("(|^)\\d{6}");
+                Matcher matcher = pattern.matcher(message);
+                if (matcher.find()) {
+                    tvOtpCode.setText(matcher.group(0));
                 }
             }
         }
-    };
+    }
+
+    private void getOtpFromMessage(String message) {
+        // This will match any 6 digit number in the message
+        Pattern pattern = Pattern.compile("(|^)\\d{6}");
+        Matcher matcher = pattern.matcher(message);
+        if (matcher.find()) {
+            tvOtpCode.setText(matcher.group(0));
+        }
+    }
+
 }
